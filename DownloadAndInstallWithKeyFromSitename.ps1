2caf25810bc03271c46d91c9382f69136b712dfb
Marc <marc.montecalvo@gmail.com>
2023-03-27 22:34:38 -0400

    Update DownloadAndInstallWithKeyFromSitename.ps1

--- DownloadAndInstallWithKeyFromSitename.ps1
+++ DownloadAndInstallWithKeyFromSitename.ps1
@@ -1,8 +1,7 @@
 $Details = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\SAAZOD)
 $sitename = $Details.sitename
-$url = @url@
-$filename = @filename@
-$Path = "C:\Windows\Temp\" + [guid]::NewGuid() + "\" + $filename
+$url = "@url@"
+$Path = "C:\Windows\Temp\" + [guid]::NewGuid() + "\@filename@"
 
 $allKeys = @{
     	'Site1' =  'Key1'
@@ -20,8 +19,8 @@ else {
     write-host "The url to use is: $url" -ForegroundColor Green
     write-host "The key to use is:"$sitekey
     write-host This is where you would do your install command using the key.
-    Invoke-WebRequest -URI $URL -OutFile ( New-Item -Path $Path -Force )
-   # $wc = New-Object System.Net.WebClient
-   # $wc.DownloadFile($url, ( New-Item -Path $Path -Force ))
+    #Invoke-WebRequest -URI $URL -OutFile ( New-Item -Path $Path -Force )
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($url, ( New-Item -Path $Path -Force ))
     Start-Process -Filepath $Path #and add your key and args
 }
